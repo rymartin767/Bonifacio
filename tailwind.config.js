4eb4125afe39a01978cb8c7f1b7dec139013deec
Ryan Martin <bills@giantpilots.com>
2020-12-03 16:20:08 -0500

    New TALL8 Project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        extend: {
+            colors: {
+                atlas_blue: '#202d61',
+                amazon_blue: '#1ab1e3',
+                atlas_yellow: '#ffb826',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+}
