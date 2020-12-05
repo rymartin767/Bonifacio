6b9688df9192c44c50d793dcba9440d27989bf9e
Ryan Martin <bills@giantpilots.com>
2020-12-05 17:48:08 -0500

    updated airlines route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/airlines', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/airlines', function () {
     return Airline::all();
 });
