43d09b12fe9e4f800df5e0e163dad1c9a7da51d6
Ryan Martin <bills@giantpilots.com>
2020-12-15 16:24:22 -0500

    updated index pagination wip

--- app/Http/Controllers/AirlineController.php
+++ app/Http/Controllers/AirlineController.php
@@ -8,12 +8,18 @@ class AirlineController extends Controller
 {
     public function index()
     {
-        $search = request('search') ?? '';
-        $hiring = request('hiring');
-        
-        return Airline::where('name', 'like', '%'.$search.'%')
-            ->where('hiring', $hiring)
-            ->get()->toJson();
+        return Airline::when(request('hiring'), function ($query) {
+            return $query
+                ->where('name', 'like', '%' . request('search') . '%')
+                ->where('hiring', true)
+                ->orderBy('updated_at', 'desc')
+                ->paginate(10);
+        }, function ($query) {
+            return $query
+                ->where('name', 'like', '%' . request('search') . '%')
+                ->orderBy('updated_at', 'desc')
+                ->paginate(10);
+        })->toJson();
     }
 
     public function show(Airline $airline)
