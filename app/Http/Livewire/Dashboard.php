43a94328f019652d6a47c57d0d4f401dd383d26e
Ryan Martin <bills@giantpilots.com>
2020-12-19 16:00:51 -0500

    updated for new livewire components

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,73 +2,11 @@
 
 namespace App\Http\Livewire;
 
-use App\Rules\ScalesFoundOnAws;
-use App\Rules\ValidateScales;
-use App\Models\Airline;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public $status;
-
-    public $sector;
-    public $name;
-    public $icao;
-    public $iata;
-    public $union;
-    public $pilots;
-    public $hiring;
-    public $url;
-
-    public function rules()
-    {
-        return [
-            'sector' => 'required|string|in:cargo,legacy,major',
-            'name' => ['required', 'string', 'unique:airlines', 'min:5', 'max:50'],
-            'iata' => 'required|unique:airlines|size:2',
-            'union' => 'required|in:alpa,apa,ipa,none,ibt,sapa',
-            'pilots' => 'required|numeric|digits_between:3,5',
-            'hiring' => 'required|boolean',
-            'icao' => ['required','unique:airlines','size:3', new ScalesFoundOnAws, new ValidateScales],
-            'url' => 'required|string|min:10|max:255'
-        ];
-    }
-
-    public function updated($propertyName)
-    {
-        $this->validateOnly($propertyName);
-    }
-
-    private function formData()
-    {
-        return [
-            'sector' => $this->sector,
-            'name' => $this->name,
-            'icao' => $this->icao,
-            'iata' => $this->iata,
-            'union' => $this->union,
-            'pilots' => $this->pilots,
-            'hiring' => $this->hiring,
-            'url' => $this->url
-        ];
-    }
-
-    public function submitForm()
-    {
-        $this->validate();
-
-        $airline = Airline::create($this->formData());
-
-        if($airline->seedScales()) {
-            $this->submitSuccess($airline->name);
-        }
-    }
-
-    private function submitSuccess($airline)
-    {
-        $this->reset();
-        $this->status = $airline . " and Scales Saved!";
-    }
+    public $selected;
 
     public function render()
     {
