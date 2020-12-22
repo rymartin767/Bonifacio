658f15706ba3c8d433191a55c090838185fe9b07
Ryan Martin <bills@giantpilots.com>
2020-12-22 15:29:34 -0500

    updated show() for server error

--- app/Http/Controllers/SeniorityController.php
+++ app/Http/Controllers/SeniorityController.php
@@ -6,41 +6,18 @@ use App\Models\Seniority;
 
 class SeniorityController extends Controller
 {
-    public function index()
-    {
-        if(request('month')) {
-            $pilots = Seniority::select(['sen', 'phire', 'emp', 'doh', 'seat', 'fleet', 'domicile', 'retire', 'month'])
-                ->where('month', request('month'))
-                ->get()->sortBy('month');
-        } else {
-            $pilots = Seniority::select(['sen', 'phire', 'emp', 'doh', 'seat', 'fleet', 'domicile', 'retire', 'month'])->get()->sortBy('month');
-        }
-        
-        if($pilots) {
-            return response()->json([
-                'status' => 201,
-                'data' => $pilots
-            ]);
-        }
-
-        return response()->json([
-            'status' => 404
-        ]);
-        
-    }
-
     public function show()
     {
         if(request('employee')) {
-            $pilot = Seniority::select(['sen', 'phire', 'emp', 'doh', 'seat', 'fleet', 'domicile', 'retire', 'month'])
+            $months = Seniority::select(['sen', 'phire', 'emp', 'doh', 'seat', 'fleet', 'domicile', 'retire', 'month'])
                 ->where('emp', request('employee'))
                 ->get()->sortBy('month');
         }
 
-        if($pilot) {
+        if($months) {
             return response()->json([
                 'status' => 201,
-                'data' => $pilot
+                'data' => $months
             ]);
         }
 
