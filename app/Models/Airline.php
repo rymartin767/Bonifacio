767c0174c2fc2524aaabf4affbd149b09c94907f
Ryan Martin <bills@giantpilots.com>
2020-12-30 17:23:36 -0500

    added SeedScales trait

--- app/Models/Airline.php
+++ app/Models/Airline.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Actions\Airlines\SeedScales;
 
 class Airline extends Model
 {
-    use HasFactory;
+    use HasFactory, SeedScales;
 
     protected $fillable = [
         'sector', 'name', 'icao', 'iata', 'union', 'pilots', 'hiring', 'url'
