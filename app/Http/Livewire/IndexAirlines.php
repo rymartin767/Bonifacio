e5cb17bcf6583be8150a25372f7513541b02435d
Ryan Martin <bills@giantpilots.com>
2020-12-30 09:38:09 -0500

    added updatedScales()

--- app/Http/Livewire/IndexAirlines.php
+++ app/Http/Livewire/IndexAirlines.php
@@ -7,6 +7,18 @@ use Livewire\Component;
 
 class IndexAirlines extends Component
 {
+    public $status = "Waiting Patiently";
+
+    public function updateScales(Airline $airline)
+    {
+        $seeded = $airline->seedScales();
+        if($seeded) {
+            $this->status = 'Updated!';
+        } else {
+            $this->status = 'Exception Caught';
+        }
+    }
+
     public function render()
     {
         return view('livewire.index-airlines', [
--- resources/views/livewire/index-airlines.blade.php
+++ resources/views/livewire/index-airlines.blade.php
@@ -1,9 +1,33 @@
 <div>
-    AIRLINES: {{ $airlines->count() }}
-    @forelse($airlines as $airline)
+    <div class="flex items-start justify-between">
         <div>
-            <div>
-                {{ $airline->name }}
+            AIRLINE COUNT: {{ $airlines->count() }}
+        </div>
+        <div>
+        <svg wire:loading wire:target="updateScales" class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+            </path>
+        </svg>
+            STATUS: {{ $status }}
+        </div>
+    </div>
+    @forelse($airlines as $airline)
+        <div class="grid grid-cols-3 gap-3">
+            <div class="col-span-1 px-2 py-3">
+                <div>
+                    {{ $airline->name }}
+                </div>
+            </div>
+            <div class="col-span-1 px-2 py-3">
+                <div>
+                    <button wire:click="updateScales({{$airline}})" type="button" class="p-2 bg-blue-500 text-white rounded shadow-sm">RESEED SCALES</button>
+                </div>
+            </div>
+            <div class="col-span-1 px-2 py-3">
+                <div>
+                    {{ $airline->scales->count() }}
+                </div>
             </div>
         </div>
     @empty  
