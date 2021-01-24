b839868258d408a4a5dd6da463685c7267e2c6d0
Ryan Martin <bills@giantpilots.com>
2021-01-24 11:03:55 -0500

    index() json response updated

--- app/Http/Controllers/AirlineController.php
+++ app/Http/Controllers/AirlineController.php
@@ -9,18 +9,23 @@ class AirlineController extends Controller
     public function index()
     {
         // returned in data key
-        return Airline::when(request('hiring'), function ($query) {
+        $airlines = Airline::when(request('hiring'), function ($query) {
             return $query
                 ->where('name', 'like', '%' . request('search') . '%')
                 ->where('hiring', true)
                 ->orderBy('updated_at', 'desc')
-                ->paginate(25);
+                ->get();
         }, function ($query) {
             return $query
                 ->where('name', 'like', '%' . request('search') . '%')
                 ->orderBy('updated_at', 'desc')
-                ->paginate(25);
-        })->toJson();
+                ->get();
+        });
+
+        return response()->json([
+            'status' => 201,
+            'data' => $airlines
+        ]);
     }
 
     public function show()
