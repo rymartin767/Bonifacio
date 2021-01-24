27e8a6313d736adaf78da263d5ddf3e2774ac526
Ryan Martin <bills@giantpilots.com>
2021-01-24 12:50:46 -0500

    removed scales routing

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/airlines', [AirlineController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/airline', [AirlineController::class, 'show'])->middleware('auth:sanctum');
-Route::get('/scales', [ScaleController::class, 'show'])->middleware('auth:sanctum');
 
 Route::get('/vacancyAwards', [VacancyController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/award', [VacancyController::class, 'show'])->middleware('auth:sanctum');
