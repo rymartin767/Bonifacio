435899a8be83e238298e28f77c5f2448f38afbc5
Ryan Martin <bills@giantpilots.com>
2021-02-06 11:04:12 -0600

    testing wip

--- app/Http/Controllers/AmesController.php
+++ app/Http/Controllers/AmesController.php
@@ -4,22 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Ame;
+use Exception;
 
 class AmesController extends Controller
 {
     public function store(Request $request)
     {
-        $ame = Ame::create($request);
+        try {
+            $ame = Ame::create($request);
 
-        if($ame) {
+            if($ame) {
+                return response()->json([
+                    'status' => 201,
+                    'data' => $ame
+                ]);
+            }
+
             return response()->json([
-                'status' => 201,
-                'data' => $ame
+                'status' => 404
             ]);
+        } catch(Exception $e) {
+            return $e;
         }
-
-        return response()->json([
-            'status' => 404
-        ]);
     }
 }
