4aacd78fa13fb5328182a4d459e97f1d62913472
Ryan Martin <bills@giantpilots.com>
2021-02-06 10:56:28 -0600

    Ame MC + routing created for storing

---
+++ app/Http/Controllers/AmesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Ame;
+
+class AmesController extends Controller
+{
+    public function store(Request $request)
+    {
+        $ame = Ame::create($request);
+
+        if($ame) {
+            return response()->json([
+                'status' => 201,
+                'data' => $ame
+            ]);
+        }
+
+        return response()->json([
+            'status' => 404
+        ]);
+    }
+}
