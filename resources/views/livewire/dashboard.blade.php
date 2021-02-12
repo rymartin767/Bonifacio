203608fd42c16f54d696bb36f6c562dadb2b97fd
Ryan Martin <bills@giantpilots.com>
2021-02-12 17:58:21 -0500

    added AME

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -78,6 +78,22 @@
                     Preview December Vacancy Award
                     </button>
                 </div>
+                <div x-data="{ open:false }">
+                    <button @click="open = !open" type="button" class="w-full inline-flex items-center px-4 py-2 text-sm font-medium text-white border-b border-gray-50 bg-indigo-600 hover:bg-indigo-700 focus:outline-none">
+                        <!-- Heroicon name: check -->
+                        <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        Medical Examiners
+                    </button>
+                    <button 
+                        x-show.transition="open" 
+                        class="w-full px-4 py-2 text-right text-sm font-medium text-white focus:outline-none bg-indigo-300 hover:bg-indigo-500 border-b border-gray-300"
+                        wire:click="$set('selected', 'index-ames')"
+                    >
+                    AME Index
+                    </button>
+                </div>
             </div>
         </div>
         <div class="col-span-10 sm:col-span-6 px-8">
@@ -102,6 +118,10 @@
 
             @if($selected === 'store-vacancy')
                 @livewire('store-vacancy')
+            @endif
+
+            @if($selected === 'index-ames')
+                @livewire('index-ames')
             @endif
         </div>
         <div class="col-span-10 sm:col-span-2">
