ba9c48dbedd1d5128a8164321a7b60bad5fea81b
Ryan Martin <bills@giantpilots.com>
2021-02-14 14:15:02 -0500

    styling wip

--- resources/views/livewire/index-airlines.blade.php
+++ resources/views/livewire/index-airlines.blade.php
@@ -1,36 +1,18 @@
-<div>
-    <div class="flex items-start justify-between">
-        <div>
-            AIRLINE COUNT: {{ $airlines->count() }}
-        </div>
-        <div>
-        <svg wire:loading wire:target="updateScales" class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-            </path>
-        </svg>
-            STATUS: {{ $status }}
-        </div>
+<div class="sm:rounded-lg sm:shadow overflow-hidden">
+    <div class="flex bg-gray-50 text-gray-500 uppercase px-6 py-3">
+        <div class="flex-1">{{ __('airline name') }}</div>
+        <div class="flex-1">{{ __('hiring?') }}</div>
+        <div class="flex-1">{{ __('scales') }}</div>
     </div>
-    @forelse($airlines as $airline)
-        <div class="grid grid-cols-3 gap-3">
-            <div class="col-span-1 px-2 py-3">
-                <div>
-                    {{ $airline->name }}
-                </div>
+    @foreach($airlines as $airline)
+        <div class="flex items-center {{ $loop->odd ? 'bg-white' : 'bg-gray-50' }} px-6 py-3">
+            <div class="flex-1">{{ $airline->name }}</div>
+            <div class="flex-1">
+                <button wire:click="toggleHiring({{$airline}})" type="button" class="p-2 {{ $airline->hiring ? 'bg-green-400' : 'bg-red-400'}} text-white rounded shadow-sm">HIRING?</button>
             </div>
-            <div class="col-span-1 px-2 py-3">
-                <div>
-                    <button wire:click="updateScales({{$airline}})" type="button" class="p-2 bg-blue-500 text-white rounded shadow-sm">RESEED SCALES</button>
-                </div>
-            </div>
-            <div class="col-span-1 px-2 py-3">
-                <div>
-                    {{ $airline->scales->count() }}
-                </div>
+            <div class="flex-1">
+                <button wire:click="updateScales({{$airline}})" type="button" class="p-2 bg-blue-500 text-white rounded shadow-sm">RESEED SCALES</button>
             </div>
         </div>
-    @empty  
-        <div>EMPTY</div>
-    @endforelse
+    @endforeach
 </div>
