505b8563958023c67293c06bb33e7315f0b1851d
Ryan Martin <bills@giantpilots.com>
2021-03-05 08:48:56 -0500

    styling wip

--- resources/views/livewire/index-ames.blade.php
+++ resources/views/livewire/index-ames.blade.php
@@ -1,5 +1,18 @@
-<div>
-    <button wire:click="truncateAmes">
-        TRUNCATE!
-    </button>
+<div class="sm:rounded-lg sm:shadow overflow-hidden">
+    <div class="flex bg-gray-50 text-sm text-gray-500 uppercase px-6 py-3">
+        <div class="flex-1">{{ __('ame') }}</div>
+        <div class="flex-1">{{ __('location') }}</div>
+        <div class="flex-1">{{ __('delete') }}</div>
+    </div>
+    @forelse($ames as $ame)
+        <div class="flex items-center {{ $loop->odd ? 'bg-white' : 'bg-gray-50' }} px-6 py-3">
+            <div class="flex-1">{{ $ame->name }}</div>
+            <div class="flex-1">{{ $ame->city }}, {{ $ame->state }}</div>
+            <div class="flex-1">
+                <button wire:click="deleteAme({{$ame->id}})" type="button" class="px-2 py-1 bg-blue-500 text-white rounded shadow-sm">DELETE</button>
+            </div>
+        </div>
+    @empty
+        <div>No Records Found.</div>
+    @endforelse
 </div>
