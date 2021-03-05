70d82fccd88bf377fc5d03e46ab1fc860288ea45
Ryan Martin <bills@giantpilots.com>
2021-03-05 10:20:47 -0500

    updated index()

--- app/Http/Controllers/AmeReviewsController.php
+++ app/Http/Controllers/AmeReviewsController.php
@@ -8,13 +8,12 @@ class AmeReviewsController extends Controller
 {
     public function index()
     {
-        if(request('ame')) {
-            $ame = Ame::find(request('ame'));
+        $ame = Ame::find(request('ame'));
+        if($ame) {
             $reviews = $ame->reviews;
-
             return response()->json(['data' => $reviews], 200);
         }
 
-        return response()->json(['data' => ['Could not find AME.']], 404);
+        return response()->json(['data' => []], 404);
     }
 }
