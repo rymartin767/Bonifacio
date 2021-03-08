a12005d7c06e502087f0838618fee51c8fbdebd5
Ryan Martin <bills@giantpilots.com>
2021-03-08 08:20:01 -0500

    added try/catch to store()

--- app/Http/Controllers/AmeReviewsController.php
+++ app/Http/Controllers/AmeReviewsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ame;
+use Exception;
 
 class AmeReviewsController extends Controller
 {
@@ -21,13 +22,17 @@ class AmeReviewsController extends Controller
     {
         $ame = Ame::find(request('ame'));
         if($ame) {
-            $review = $ame->reviews()->create([
-                'rating' => request('rating'),
-                'comment' => request('comment'),
-                'emp_id' => request('reviewer')
-            ]);
+            try {
+                $review = $ame->reviews()->create([
+                    'rating' => request('rating'),
+                    'comment' => request('comment'),
+                    'emp_id' => request('reviewer')
+                ]);
 
-            return response()->json(['data' => $review], 201);
+                return response()->json(['data' => $review], 201);
+            } catch (Exception $e) {
+                return response()->json(['data' => []], 400); // code 400 is a bad request
+            }
         }
 
         return response()->json(['data' => []], 404);
