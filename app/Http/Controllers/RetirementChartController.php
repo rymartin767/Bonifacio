c52dd7f4e1c4c96834aeb95f89d8942b80d999a1
Ryan Martin <bills@giantpilots.com>
2021-04-06 12:21:26 -0400

    initial create

---
+++ app/Http/Controllers/RetirementChartController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Seniority;
+use Carbon\Carbon;
+use Exception;
+
+class RetirementChartController extends Controller
+{
+    public function __invoke()
+    {
+        $month = Seniority::pluck('month')->unique()->sort()->last();
+        $current_list = Seniority::where('month', $month)->get();
+
+        try {
+            $collection = $current_list->groupBy(function ($item, $key) {
+                return Carbon::parse($item['retire'])->format('Y');
+            })->map->count();
+            
+            return response()->json(['data' => $collection->all()], 200);
+        } catch(Exception $e) {
+            return response()->json(['data' => []], 404);
+        }
+    }
+}
