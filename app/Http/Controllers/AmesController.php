d49c8bdfa06aa95f0dd446570fa7375e0ef7cdeb
Ryan Martin <bills@giantpilots.com>
2021-04-14 17:01:09 -0400

    updated array in state validation

--- app/Http/Controllers/AmesController.php
+++ app/Http/Controllers/AmesController.php
@@ -13,7 +13,14 @@ class AmesController extends Controller
             'name' => ['required', 'string', 'min:5', 'max:50', 'regex:/^([^0-9]*)$/'],
             'street' => ['required', 'string', 'min:5', 'max:50'],
             'city' => ['required', 'string', 'min:2', 'max:75'],
-            'state' => ['required', Rule::in($this->states)],
+            'state' => ['required', Rule::in(
+                [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 
+                    'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 
+                    'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 
+                    'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 
+                    'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 
+                    'WA', 'WV', 'WI', 'WY' ]
+            )],
             'zip' => ['required', 'numeric', 'digits:5'],
             'phone' => ['required', 'regex:/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/'],
         ]);
