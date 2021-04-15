4c0838061362fe42a6a55931c0d00f28feeccbdf
Ryan Martin <bills@giantpilots.com>
2021-04-15 11:39:04 -0400

    updated ames delete route

--- routes/api.php
+++ routes/api.php
@@ -58,7 +58,7 @@ Route::get('employeeRate', [EmployeeScalesController::class, 'show'])->middlewar
 
 Route::post('ames', [AmesController::class, 'store'])->middleware('auth:sanctum');
 Route::get('ames', [AmesController::class, 'index'])->middleware('auth:sanctum');
-Route::delete('ames', [AmesController::class, 'destroy'])->middleware('auth:sanctum');
+Route::delete('ames/{id}', [AmesController::class, 'destroy'])->middleware('auth:sanctum');
 
 Route::get('reviews', [AmeReviewsController::class, 'index'])->middleware('auth:sanctum');
 Route::post('reviews', [AmeReviewsController::class, 'store'])->middleware('auth:sanctum');
