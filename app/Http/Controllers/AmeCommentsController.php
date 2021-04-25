011244ae0d87da7a96383682862e53e44bb6acbb
Ryan Martin <bills@giantpilots.com>
2021-04-25 14:51:17 -0700

    removed index()

--- app/Http/Controllers/AmeCommentsController.php
+++ app/Http/Controllers/AmeCommentsController.php
@@ -3,22 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ame;
-use Exception;
 use Illuminate\Validation\ValidationException;
 
 class AmeCommentsController extends Controller
 {
-    public function index()
-    {
-        $ame = Ame::find(request('ame'));
-        if($ame) {
-            $comments = $ame->comments;
-            return response()->json(['data' => $comments], 200);
-        }
-
-        return response()->json(['data' => []], 404);
-    }
-
     public function store()
     {
         $ame = Ame::find(request('ame_id'));
