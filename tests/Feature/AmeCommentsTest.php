b429ea1de6c1789925f1905909673725237de2c8
Ryan Martin <bills@giantpilots.com>
2021-05-05 08:20:43 -0400

    updated + passing

--- tests/Feature/AmeCommentsTest.php
+++ tests/Feature/AmeCommentsTest.php
@@ -3,22 +3,46 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use App\Models\AmeRating;
+use Laravel\Sanctum\Sanctum;
+use App\Models\User;
 use Tests\TestCase;
+use App\Models\Ame;
 
 class AmeCommentsTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_a_404_response_is_returned_if_ame_is_not_found()
+    private function asSanctum()
     {
-        $response = $this->postJson('/api/ameComments', AmeRating::factory()->raw(['user_id' => 434]));
-        $response->assertExactJson(['data' => []], 404);
+        return Sanctum::actingAs(
+            User::factory()->create(),
+            ['*']
+        );
+    }
+
+    public function test_a_422_response_is_returned_if_ame_model_is_not_found()
+    {
+        $this->asSanctum();
+
+        $response = $this->postJson('/api/ames/1/comments', ['user_id' => 1, 'body' => 'Testing the ames comments api']);
+        $response->assertExactJson(['data' => ['AME Model Not Found!']], 422);
     }
 
     public function test_a_422_response_is_returned_if_validation_exception_is_caught_during_storage()
     {
-        $response = $this->postJson('/api/ameComments', AmeRating::factory()->raw(['user_id' => 'string']));
-        $response->assertExactJson(['data' => []], 422);
+        $this->asSanctum();
+
+        $response = $this->postJson('/api/ames/1/comments', ['user_id' => 1, 'body' => 'api']);
+        $response->assertExactJson(['data' => ["The given data was invalid."]], 422);
+    }
+
+    public function test_a_201_response_is_returned_if_ame_comment_is_stored()
+    {
+        $this->asSanctum();
+
+        Ame::factory()->create();
+
+        $response = $this->postJson('/api/ames/1/comments', ['user_id' => 1, 'body' => 'Testing the ames comments api']);
+        $response->assertExactJson(['data' => ['success']], 201);
     }
 }
