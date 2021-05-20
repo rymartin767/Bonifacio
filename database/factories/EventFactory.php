371cd0dde39172c9d3debf5aa8f1d60e93894922
Ryan Martin <bills@giantpilots.com>
2021-05-20 09:06:20 -0700

    created for testing

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'title' => 'Event Title',
+            'datetime' => now(),
+            'image' => $this->faker->imageUrl(),
+            'url' => $this->faker->url
+        ];
+    }
+}
